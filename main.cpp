ea5addec6d8fe23313498d18ab125d930cdf3e08
OskarSINF <oskar.szubert@gmail.com>
2016-04-13 21:33:50 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,31 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	srand(atoi(argv[2]));
+	int n=atoi(argv[1]);
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square=NULL;
+	square=new char *[n];
+	for(int i=0;i<n;i++)
+		square[i]=new char[n];
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+	for(int i=0;i<n*n;i++)
+	{
+		square[i/n][i%n]=rand()%26+97;
+		printf("%c ",square[i/n][i%n]);
+		if((i+1)%n==0)
+			printf("\n");
+	}
 
+	for(int i=0;i<n;i++)
+		delete[]square[i];
+	delete[]square;
 }
